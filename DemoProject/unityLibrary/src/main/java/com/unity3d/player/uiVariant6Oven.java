49d24510240a246b9ff520c237ee3b5112cc066a
Nam Ho Koh <lucaskoh.lk@gmail.com>
2020-08-13 22:46:29 -0700

    Added logic to oven panel.

--- DemoProject/unityLibrary/src/main/java/com/unity3d/player/uiVariant6Oven.java
+++ DemoProject/unityLibrary/src/main/java/com/unity3d/player/uiVariant6Oven.java
@@ -2,7 +2,19 @@ package com.unity3d.player;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class uiVariant6Oven extends AppCompatActivity {
 
@@ -10,5 +22,43 @@ public class uiVariant6Oven extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ui_variant6_oven);
+
+        Button next = findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextActivity();
+            }
+        });
+    }
+
+    private void nextActivity() {
+        int incoming_index = TaskInstructionActivity.indexBundle.getInt("index");
+        HashMap<String, String> tmpHash = getData();
+        ArrayList<String> instructionList = new ArrayList<>(tmpHash.values());
+        if (incoming_index < instructionList.size() - 1) {
+            Intent intent = new Intent(this, TaskInstructionActivity.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, UserSurveyActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private HashMap<String, String> getData() {
+        InputStream ls = getResources().openRawResource(R.raw.file2);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(ls, StandardCharsets.UTF_8));
+        String line = "";
+        HashMap<String, String> resultList = new HashMap<String, String>();
+        try {
+            while ((line = reader.readLine()) != null) {
+                String[] tokens = line.split("\t");
+                resultList.put(tokens[0], tokens[1]);
+            }
+        } catch (IOException e) {
+            Log.wtf("TaskInstructionActivity", "Error reading data file on line" + line, e);
+            e.printStackTrace();
+        }
+        return resultList;
     }
 }
