6b919e5fa613d0b27f5fe662ce9cffbbe1a93156
Nam Ho Koh <lucaskoh.lk@gmail.com>
2020-08-13 22:31:12 -0700

    Updating android target.

--- DemoProject/unityLibrary/src/main/java/com/unity3d/player/UserConsentActivity.java
+++ DemoProject/unityLibrary/src/main/java/com/unity3d/player/UserConsentActivity.java
@@ -19,7 +19,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
-import com.aic.libnilu.*;
+//import com.aic.libnilu.*;
 
 public class UserConsentActivity extends AppCompatActivity {
 
