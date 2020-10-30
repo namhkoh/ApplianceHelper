46aa22faea01f7da2fd73e2742aa0278614cdd4f
namhkoh <43123189+namhkoh@users.noreply.github.com>
2020-10-30 16:06:25 +0000

    Added draggable button.

--- DemoProject/unityLibrary/src/main/java/com/unity3d/player/uiVariant4WithMicrowave.java
+++ DemoProject/unityLibrary/src/main/java/com/unity3d/player/uiVariant4WithMicrowave.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -34,6 +35,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.Debug;
 import androidx.core.content.ContextCompat;
 
 import com.aic.libnilu.nlu.MainManager;
@@ -56,13 +58,44 @@ import java.util.Objects;
  * this class will contain the microwave panel with the speech button.
  */
 
-public class uiVariant4WithMicrowave extends AppCompatActivity {
+public class uiVariant4WithMicrowave extends AppCompatActivity implements View.OnTouchListener {
 
+    private float dX;
+    private float dY;
+    private int lastAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ui_variant4_with_microwave);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        View draggableView = findViewById(R.id.speechButton);
+        draggableView.setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastAction = MotionEvent.ACTION_DOWN;
+                dX = v.getX() - event.getRawX();
+                dY = v.getY() - event.getRawY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                lastAction = MotionEvent.ACTION_MOVE;
+                v.setX(event.getRawX() + dX);
+                v.setY(event.getRawY() + dY);
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.e("buttonState","Pressed");
+                break;
+        }
+        return true;
+    }
+
+
+    private void showToast(String inputText) {
+        Toast.makeText(this, inputText, Toast.LENGTH_SHORT).show();
     }
 }
