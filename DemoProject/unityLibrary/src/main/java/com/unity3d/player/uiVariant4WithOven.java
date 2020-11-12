b339ade4f8fa16a0683e1eeeec85de28eb5bd4e5
namhkoh <43123189+namhkoh@users.noreply.github.com>
2020-11-12 19:03:55 +0000

    Added uiVariant4-5 Oven panels.

---
+++ DemoProject/unityLibrary/src/main/java/com/unity3d/player/uiVariant4WithOven.java
@@ -0,0 +1,14 @@
+package com.unity3d.player;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class uiVariant4WithOven extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui_variant4_with_oven);
+    }
+}
