03d5b7a712f3a0de5d51c7933115cdeb35c2fdd6
namhkoh <43123189+namhkoh@users.noreply.github.com>
2021-01-07 09:08:06 +0900

    Added data confirmation screen

---
+++ DemoProject/unityLibrary/src/main/java/com/unity3d/player/EndActivityScreen.java
@@ -0,0 +1,14 @@
+package com.unity3d.player;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EndActivityScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_screen);
+    }
+}
